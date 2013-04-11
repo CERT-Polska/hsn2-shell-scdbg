b2908664afec781753e6d194ccdf81aaab765233
Michal Michalak <michal.michalak@nask.pl>
2013-04-11 09:34:05 +0200

    Version update. (updates CERT-Polska/hsn2-bundle#2)

--- src/test/java/pl/nask/hsn2/service/scdbg/ScdbgMockTest.java
+++ src/test/java/pl/nask/hsn2/service/scdbg/ScdbgMockTest.java
@@ -36,9 +36,7 @@ import org.testng.annotations.Test;
 
 import pl.nask.hsn2.ResourceException;
 
-@Test
 public class ScdbgMockTest {
-
     private File tmpDir;
     private File bPdfFile;
     private File tPdfFile;
@@ -81,7 +79,7 @@ public class ScdbgMockTest {
         builder = new ScdbgResultBuilder();
     }
 
-    @Test
+    @Test(enabled=false)
     public void scanForOffsetsTest() throws ResourceException {
         tool.scanForOffsets(builder, bPdfFile.getAbsolutePath());
 
@@ -112,7 +110,7 @@ public class ScdbgMockTest {
         Assert.assertTrue(offset.getGraphFile() != null);
     }
 
-    @Test
+    @Test(enabled=false)
     public void scanOffsetWithDumpFile() throws ResourceException, FileNotFoundException, IOException {
         tool.scanOffset(builder, fileWithMemDump, new Offset("0x012c", "40c538"));
 
